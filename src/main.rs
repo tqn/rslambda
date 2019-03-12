b83fdc902d4995b404d5928f1b13074c7313b3ca
Tej Qu Nair <tej.q.nair@gmail.com>
2019-03-11 22:22:02 -0700

    Basic beta/eta reduction

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::collections::HashMap;
-use std::fmt;
+use std::{collections::HashMap, fmt, mem};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Exp<T> {
     // variable
     Var(T),
@@ -19,32 +18,32 @@ impl Exp<String> {
     ) -> Result<(), String> {
         // scope maps original variable name to unique id
         match self {
-            Exp::Var(v) => {
+            Exp::Var(name) => {
                 // try to replace with the new identifier at the top of the stack
-                if let Some(identifiers) = scope.get(v) {
-                    if let Some(id) = identifiers.last() {
-                        *v = id.clone();
+                if let Some(identifiers) = scope.get(name) {
+                    if let Some(new_id) = identifiers.last() {
+                        *name = new_id.clone();
                     }
                 }
             }
-            Exp::Abs(v, b) => {
+            Exp::Abs(var, body) => {
                 // construct an empty vec if it is missing
-                if !scope.contains_key(v) {
-                    scope.insert(v.clone(), Default::default());
+                if !scope.contains_key(var) {
+                    scope.insert(var.clone(), Default::default());
                 }
                 // scope in
-                let id = get_unique_identifier(&v, symbols);
-                scope.get_mut(v).unwrap().push(id.clone());
+                let id = get_unique_identifier(&var, symbols);
+                scope.get_mut(var).unwrap().push(id.clone());
                 // convert the body
-                b.make_identifiers_unique(symbols, scope)?;
+                body.make_identifiers_unique(symbols, scope)?;
                 // remove our identifier, it can never be referenced again
-                let idents = scope.get_mut(v).unwrap();
+                let idents = scope.get_mut(var).unwrap();
                 idents.pop();
                 // shrink the vec if it's too big (premature optimization?)
                 if idents.len() <= idents.capacity() / 2 {
                     idents.shrink_to_fit();
                 }
-                *v = id;
+                *var = id;
             }
             Exp::App(l, r) => {
                 l.make_identifiers_unique(symbols, scope)?;
@@ -52,6 +51,79 @@ impl Exp<String> {
             }
         };
         Ok(())
+    }
+
+    // returns a boolean indicating whether any changes were made
+    fn beta_reduce(&mut self) -> bool {
+        // beta reduce from bottom up one time to avoid infinite regress
+        match self {
+            // where everything interesting happens
+            Exp::App(func, arg) => {
+                // if the application has an abstraction on the left
+                if let Exp::Abs(var, body) = func.as_ref() {
+                    let mut reduced = body.as_ref().clone();
+                    // bottom-up beta reduce
+                    reduced.beta_reduce();
+                    reduced.substitute(var, arg);
+                    mem::replace(self, reduced);
+                    true
+                } else {
+                    // otherwise reduce both sides
+                    // use temp variable to avoid short-circuit eval
+                    let changed = func.beta_reduce();
+                    arg.beta_reduce() || changed
+                }
+            }
+            Exp::Abs(_, body) => {
+                // recurse
+                body.beta_reduce()
+            }
+            // do nothing
+            _ => false,
+        }
+    }
+
+    fn eta_reduce(&mut self) -> bool {
+        match self {
+            Exp::App(func, arg) => {
+                let changed = func.eta_reduce();
+                arg.eta_reduce() || changed
+            }
+            Exp::Abs(var, body) => {
+                let changed = body.eta_reduce();
+                if let Exp::App(l, r) = body.as_ref() {
+                    if let Exp::Var(v) = r.as_ref() {
+                        if *var == *v {
+                            let mut reduced = l.as_ref().clone();
+                            reduced.eta_reduce();
+                            mem::replace(self, reduced);
+                            return true;
+                        }
+                    }
+                }
+                changed
+            }
+            _ => false,
+        }
+    }
+
+    fn substitute(&mut self, symbol: &str, exp: &Exp<String>) {
+        // TODO make unique vars for every bound variable in the
+        match self {
+            Exp::App(l, r) => {
+                l.substitute(symbol, exp);
+                r.substitute(symbol, exp);
+            }
+            Exp::Abs(_, body) => {
+                // should be made unique already, so binding is ignored
+                body.substitute(symbol, exp);
+            }
+            Exp::Var(id) => {
+                if *id == symbol {
+                    *self = exp.clone();
+                }
+            }
+        }
     }
 }
 
@@ -76,16 +148,20 @@ where
 }
 
 fn main() {
-    let input = r"\f.(\x.f(x x))(\x.f(x x))";
+    // let input = r"\f.(\x.f(x x))(\x.f(x x))";
+    let input = r"(\x.\y.f x y)";
 
     // TEMP: one char tokens, ignore whitespace
     let mut tokens = lex(input);
     preprocess(&mut tokens);
     // make tokens immutable
     let tokens = tokens.iter().map(String::as_str).collect::<Vec<_>>();
-    let program = parse(&tokens);
+    let mut program = parse(&tokens).unwrap();
 
-    println!("{}", program.unwrap());
+    program.beta_reduce();
+    program.eta_reduce();
+
+    println!("{}", program);
 }
 
 fn lex(input: &str) -> Vec<String> {
